a829241b1a9beea0bceb4d3af29407d74f25f1eb
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2025-01-10 14:29:39 +0100

    fixes

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useMotionValueEvent, useScroll } from 'framer-motion'
+import { useMotionValueEvent, useScroll } from 'motion/react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useTranslations } from 'next-intl'
